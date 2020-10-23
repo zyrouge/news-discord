56c7b0037a81b622b857f6386ad9787d14d58392
ZYROUGE <me@zyrouge.com>
2020-10-23 20:06:46 +0530

    hmm

--- scripts/workflow.js
+++ scripts/workflow.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 
-module.exports = (actionID, version, sha) => {
+module.exports = (actionID, version, sha, event) => {
     fs.writeFileSync(
         "./README.md",
         [
             `This is a automatic build of [\`bot\`](https://github.com/zyrouge/news-discord/tree/bot) branch using Github Actions`,
             `## Build Info`,
             `* Action ID - \`${actionID}\``,
+            `* Event - \`${event}\``,
             `* Version - \`${version}\``,
             `* Commit SHA - \`${sha}\``
         ].join("\n")
