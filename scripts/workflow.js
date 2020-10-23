c256476874ccb5c4424000c634367c3fd1d5466a
ZYROUGE <me@zyrouge.com>
2020-10-23 20:22:38 +0530

    Hopefully last push today

--- scripts/workflow.js
+++ scripts/workflow.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 
-module.exports = (actionID, version, sha, event) => {
+module.exports = ({ run_id, version, sha, event, pusher }) => {
     fs.writeFileSync(
         "./README.md",
         [
+            `# News Bot (Production Code)`,
             `This is a automatic build of [\`bot\`](https://github.com/zyrouge/news-discord/tree/bot) branch using Github Actions`,
             `## Build Info`,
-            `* Action ID - \`${actionID}\``,
+            `* Runner ID - \`${run_id}\``,
             `* Event - \`${event}\``,
             `* Version - \`${version}\``,
-            `* Commit SHA - \`${sha}\``
+            `* Commit SHA - \`${sha}\``,
+            `* Pusher: \`${pusher.name}\``,
+            `* Time of Built: **${new Date().toLocaleString()}**`
         ].join("\n")
     );
 };
